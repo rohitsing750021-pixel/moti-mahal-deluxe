9021954f3cf675475aed72a119842a7529ecde0d
rohitsing750021-pixel <rohitsing750021@gmail.com>
2026-07-23 21:09:01 +0530

    Add alert for reservation form submission

---
+++ script.js
@@ -0,0 +1,5 @@
+// Reservation form alert
+document.querySelector(".reservation-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your table reservation request has been submitted.");
+});
